62cf15b405e834568ace876b43a012658f7be91b
Joseph Mtinangi <josephmtinangi@gmail.com>
2023-12-31 09:10:04 +0800

    Clip bottom right corner

--- app/src/main/java/tz/co/jtech/schoolieparent/presentation/common/AuthCard.kt
+++ app/src/main/java/tz/co/jtech/schoolieparent/presentation/common/AuthCard.kt
@@ -7,10 +7,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,6 +32,7 @@ fun AuthCard(
         modifier = Modifier
             .fillMaxWidth()
             .height(180.dp)
+            .clip(RoundedCornerShape(bottomEnd = 70.dp))
             .background(Blue),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
